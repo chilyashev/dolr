113299d169dcef529f39cc641b3dcccee76367aa
Mihail Chilyashev <chilyashev@gmail.com>
2020-11-30 15:46:30 +0200

    Return old version

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,62 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm6 md4 lg4>
-      <div class="display-4 font-weight-black text-center">Не!</div>
-      <div class="mb-2">
-        <p>Ако имаш възможност, остани си вкъщи.</p>
-        <p>Липсват ли ти по-простите времена? Кликни <nuxt-link to='/forgotten-times'>тук</nuxt-link>, за да си ги припомниш.</p>
-      </div>
-      <small> (<a @click='butVisible = !butVisible'>Ама аз съм безсмъртен, млад, здрав, пия ракия, нищо няма да ми стане!</a>)</small>
-   
-    </v-flex> 
-     <v-sheet class="pa-9" elevation="1" v-show='butVisible'>
-       Причини да си останеш вкъщи:
-       <ol>
-         <li>Без трафик към работата!</li>
-         <li>Може да заразиш други.</li>
-         <li>Може да наваксаш с всичко вкъщи, което отлагаш толкова време. </li>
-         <li>Има изследвания, които твърдят, че след прекарване на инфекцията остават белези по белия дроб и са възможни трайни увреждания. Source: <a href="https://www.sciencealert.com/even-those-who-recover-from-corona-can-be-left-gasping-for-breath-afterwards">https://www.sciencealert.com/even-those-who-recover-from-corona-can-be-left-gasping-for-breath-afterwards</a></li>
-         <li>Може да намалиш контактите с други хора!</li>
-         <li>Може да прекараш всички работни срещи само по бельо<abbr title="Ако забравиш да спреш камерата, ще е забавно за всички">*</abbr>!</li>
-         <li>Без трафик към вкъщи!</li>
-       </ol>
-       
+      <v-sheet class="pa-9" elevation="1">
+        <p>Всеки ден този въпрос си задават милиони българи.</p>
+        <p>
+          Благодарение на напреднал изкуствен интелект и естествена липса на желание за работа създадохме платформа,
+          която ще ви помогне да вземете правилното решение!
+          <br />Тъй като най-вероятно
+          <del>бързате</del> закъснявате за работа, предлагаме две решения:
+        </p>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-card tile>
+              <v-card-title>Бързо решение&trade;</v-card-title>
+              <v-card-text>
+                <ul>
+                  <li>Отнема малко време</li>
+                  <li>100% вярно в част от случаите</li>
+                  <li>Използва само 100 грама блокчейн</li>
+                </ul>
+              </v-card-text>
+            </v-card>
+            <v-btn color="primary" nuxt to="/42" block>Към бързото решение</v-btn>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-card tile>
+              <v-card-title>Подробен тест</v-card-title>
+              <v-card-text>
+                <ul>
+                  <li>Отнема малко повече време</li>
+                  <li>100% вярно в същата част от случаите</li>
+                  <li>Използва 250 грама чист био-блокчейн</li>
+                </ul>
+              </v-card-text>
+            </v-card>
+            <v-btn color="primary" nuxt to="/assess" block>Към теста</v-btn>
+          </v-col>
+        </v-row>
+        <v-row class="mt-9">
+          <v-col>
+            <h3>Помощ! Отидох на работа! Какво да правя сега?</h3>
+            Спокойно, погрижили сме се и за вашия случай. Заповядяйте към <nuxt-link to="gishe_4">гише №4</nuxt-link>.
+          </v-col>
+        </v-row>
       </v-sheet>
-        
+      <p>
+        <small class="text-left">* Бързото решение може да ви изненада!</small>
+        <br />
+        <small
+          class="text-left"
+        >** Ако днес сте се минали да отидете на работа, опитайте теста отново утре. А дотогава може да опитате <a href="https://lmgtfy.com/?q=%D0%BF%D0%B0%D1%81%D0%B8%D0%B0%D0%BD%D1%81+%D0%B8+%D0%BC%D0%B8%D0%BD%D0%B8%D1%87%D0%BA%D0%B8">това</a>.</small>
+      </p>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
-  components: {},
-  data(){
-    return {
-      butVisible: false
-    }
-  },
-  methods: {
-
-  }
+  components: {}
 };
 </script>
